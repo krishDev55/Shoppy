60a0c60c6029b1356bdb0998290ae15738f3212f
krishDev55 <kd.dev.enr.55@gmail.com>
2025-03-22 12:27:08 +0530

    Update ShoppyCartApplicationTests.java

--- src/test/java/com/ShoppyCart/ShoppyCartApplicationTests.java
+++ src/test/java/com/ShoppyCart/ShoppyCartApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 /****
 	This class is use for Testing purpose, like 
  		junitTesting ,
-
+But i have Problem How to Add TestCase in my Class 
 
 **/
 @SpringBootTest
