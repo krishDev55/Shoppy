6968dfd2ff184aab8776a8ae6b03b4ec021c8fd2
krishDev55 <kd.dev.enr.55@gmail.com>
2025-03-22 12:15:49 +0530

    Update ShoppyCartApplicationTests.java
    massage is Add the Testing purpose of Test class

--- src/test/java/com/ShoppyCart/ShoppyCartApplicationTests.java
+++ src/test/java/com/ShoppyCart/ShoppyCartApplicationTests.java
@@ -3,6 +3,12 @@ package com.ShoppyCart;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/****
+	This class is use for Testing purpose, like 
+ 		junitTesting ,
+
+
+**/
 @SpringBootTest
 class ShoppyCartApplicationTests {
 
